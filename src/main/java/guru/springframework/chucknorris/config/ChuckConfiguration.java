1fac6781c7a0ac38d06c92f790f0c51a93f8613b
Ryan Williams <ryan.williams@projectpeople.com>
2017-10-30 16:34:22 +0000

    Completed Lesson 60 - done some XML configuration.

--- src/main/java/guru/springframework/chucknorris/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/chucknorris/config/ChuckConfiguration.java
@@ -4,10 +4,10 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
-
-    @Bean
+//commenting out to do an XML configuration - see resources/chuck-config.xml and ChucknorrisApplication.java:@ImportResource("classpath:chuck-config.xml")
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
